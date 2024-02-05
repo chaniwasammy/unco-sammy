f800de54393a74dc04c0754ac6159579abd4a52e
sammychaniwa <chaniwasammy@gmail.com>
2024-02-05 14:01:47 +0200

    first commit

--- src/pages/Donate.js
+++ src/pages/Donate.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import PazDonateTODAY from '/Users/uncommonstudent/Desktop/react project/uncommon project/src/components/PazDonateTODAY.js'
-import PazMorepages from '/Users/uncommonstudent/Desktop/react project/uncommon project/src/components/PazMorepages.js'
-import PazFooter from '/Users/uncommonstudent/Desktop/react project/uncommon project/src/components/PazFooter.js'
-import PazBluesec from '/Users/uncommonstudent/Desktop/react project/uncommon project/src/components/PazBluesec.js'
-import PazHeader from '/Users/uncommonstudent/Desktop/react project/uncommon project/src/components/PazHeader.js'
+import PazDonateTODAY from '../components/PazDonateTODAY'
+import PazMorepages from '../components/PazMorepages'
+import PazFooter from '../components/PazFooter'
+import PazBluesec from '../components/PazBluesec'
+import PazHeader from '../components/PazHeader'
 
 function Donate() {
   return (

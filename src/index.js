6d7063d06f8fe93870a034db2f7987e47af5bebc
sammychaniwa <chaniwasammy@gmail.com>
2024-02-05 13:51:07 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { ThemeProvider } from 'styled-components';
-import reportWebVitals from './reportWebVitals';
 import GlobalStyle from './theme/GlobalStyle';
 import theme from "./theme/theme";
 
@@ -20,4 +19,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
